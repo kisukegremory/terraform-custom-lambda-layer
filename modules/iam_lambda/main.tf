32f1905f933f603d08882275d1cd3630c46a20b9
Gustavo de Souza Gonçalves <goncalves.gustavo211@gmail.com>
2024-06-06 14:00:50 -0300

    feat: created iam module for lambda permissions

---
+++ modules/iam_lambda/main.tf
@@ -0,0 +1,54 @@
+variable "project_name" {
+  type = string
+}
+
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+    sid    = "CloudwatchId"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "this" {
+  name        = "${var.project_name}-policy"
+  description = "Policy to allow Cloudwatch Usage to a specific resource"
+  policy      = data.aws_iam_policy_document.this.json
+}
+
+
+resource "aws_iam_role" "this" {
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  name               = "${var.project_name}-role"
+}
+
+resource "aws_iam_policy_attachment" "this" {
+  name = "${var.project_name}-role-attachment"
+  roles = [
+    aws_iam_role.this.name,
+  ]
+  policy_arn = aws_iam_policy.this.arn
+}
+
+
